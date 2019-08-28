b3c887af425a1108b6813c849fb3b693956f77a8
konstantinosx <kostasx@cs.umd.edu>
2019-08-28 07:54:40 -0400

    YOUR CODE HERE comments

--- src/process_pool_launcher.cc
+++ src/process_pool_launcher.cc
@@ -14,69 +14,69 @@ process_pool_launcher::process_pool_launcher(uint32_t nprocs)
         sem_t *proc_sems;
         proc_state *pstates;
 
-        /* 
-         * Setup launcher state. Initialize the launcher's proc_mgr struct. 
+        /*
+         * Setup launcher state. Initialize the launcher's proc_mgr struct.
          */
         _launcher_state = (proc_mgr*)mmap(NULL, sizeof(proc_mgr), PROT_FLAGS, MAP_FLAGS, 0, 0);
         temp_sem = (sem_t*)mmap(NULL, 2*sizeof(sem_t), PROT_FLAGS, MAP_FLAGS, 0, 0);
-        
+
         /*
-         * _done_sem is used to track the number of idle processes. 
-         * process_pool_launcher's use of _done_sem is similar to 
-         * process_launcher's use of _done_sem to control the number of 
+         * _done_sem is used to track the number of idle processes.
+         * process_pool_launcher's use of _done_sem is similar to
+         * process_launcher's use of _done_sem to control the number of
          * outstanding processes
          */
         _launcher_state->_done_sem = temp_sem;
-        
-        /* 
+
+        /*
          * YOUR CODE HERE
-         * 
-         * Initialize _launcher_state->_done_sem with an appropriate starting 
+         *
+         * Initialize _launcher_state->_done_sem with an appropriate starting
          * value using sem_init.
-         * 
-         * Hint: Since _done_sem is shared among multiple processes, its second 
+         *
+         * Hint: Since _done_sem is shared among multiple processes, its second
          * argument must be set appropriately.
          */
 
 
-        /* 
-         * _free_list_sem is used as a mutex lock. It protects the free-list of 
-         * idle processes from concurrent modifications. 
+        /*
+         * _free_list_sem is used as a mutex lock. It protects the free-list of
+         * idle processes from concurrent modifications.
          */
-        _launcher_state->_free_list_sem = &temp_sem[1];  
-        
-        /* 
+        _launcher_state->_free_list_sem = &temp_sem[1];
+
+        /*
          * YOUR CODE HERE
-         * 
-         * Initialize _free_list_sem with an appropriate starting value using 
+         *
+         * Initialize _free_list_sem with an appropriate starting value using
          * sem_init.
-         * 
-         * Hint: Since _free_list_sem is shared among multiple processes, its 
+         *
+         * Hint: Since _free_list_sem is shared among multiple processes, its
          * second argument must be set appropriately.
          */
 
-        
-        /* 
-         * Setup request buffers. Each process in the pool has its own private 
-         * request buffer. In order to assign a request to a process the 
-         * launcher process must copy the request into the process' request 
+
+        /*
+         * Setup request buffers. Each process in the pool has its own private
+         * request buffer. In order to assign a request to a process the
+         * launcher process must copy the request into the process' request
          * buffer.
          */
         req_bufs = (char*)mmap((NULL), nprocs*RQST_BUF_SZ, PROT_FLAGS, MAP_FLAGS, 0, 0);
-        
-        /* 
-         * Each process in the pool has a corresponding semaphore which 
-         * is used to signal the process to begin executing a new request. 
+
+        /*
+         * Each process in the pool has a corresponding semaphore which
+         * is used to signal the process to begin executing a new request.
          * We initialize the value of this semaphore to 0.
          */
         proc_sems = (sem_t*)mmap(NULL, nprocs*sizeof(sem_t), PROT_FLAGS, MAP_FLAGS, 0, 0);
-        for (i = 0; i < nprocs; ++i) 
+        for (i = 0; i < nprocs; ++i)
                 sem_init(&proc_sems[i], INTER_PROC_SEM, 0);
 
-        /* 
-         * Setup proc_states for each process in the pool. proc_states are 
-         * linked via the _list_ptr field. The free-list of proc_states is 
-         * stored in _launcher_state->_free_list. 
+        /*
+         * Setup proc_states for each process in the pool. proc_states are
+         * linked via the _list_ptr field. The free-list of proc_states is
+         * stored in _launcher_state->_free_list.
          */
         pstates = (proc_state*)mmap(NULL, sizeof(proc_state)*nprocs, PROT_FLAGS, MAP_FLAGS, 0, 0);
         for (i = 0; i < nprocs; ++i) {
@@ -89,10 +89,10 @@ process_pool_launcher::process_pool_launcher(uint32_t nprocs)
         pstates[i-1]._list_ptr = NULL;
         _launcher_state->_free_list = pstates;
 
-        /* 
+        /*
          * YOUR CODE HERE
-         * 
-         * Launch the processes in the process pool. 
+         *
+         * Launch the processes in the process pool.
          */
 }
 
@@ -101,20 +101,23 @@ void process_pool_launcher::executor_fn(proc_state *st)
         while (true) {
                 /*
                  * YOUR CODE HERE
-                 * 
-                 * Wait for a new request, and execute it. After executing the 
-                 * request return proc_state to the launcher's proc_state 
+                 *
+                 * Wait for a new request, and execute it. After executing the
+                 * request return proc_state to the launcher's proc_state
                  * free-list. Remember to signal/wait the appropriate _proc_sem,
                  * and _done_sem.
-                 * 
-                 * When your code is ready, remove the assert(false) statement 
+                 *
+                 * When your code is ready, remove the assert(false) statement
                  * below.
                  */
                 assert(false);
-           
+
                 st->_request->execute();
                 fetch_and_increment(st->_txns_executed);
 
+                /*
+                 * YOUR CODE HERE
+                 */
         }
         exit(0);
 }
@@ -126,14 +129,14 @@ void process_pool_launcher::execute_request(request *req)
         launcher::execute_request(req);
 
         st = NULL;
-        /* 
+        /*
          * YOUR CODE HERE
-         * 
-         * Find an idle process from _free_list, copy the request into the 
+         *
+         * Find an idle process from _free_list, copy the request into the
          * process' request buffer, and execute the request on the idle process.
-         * 
-         * Hint: Use the process' _proc_sem, and the launcher's _done_sem to 
-         * initiate a new request on the idle process, and ensure that there 
+         *
+         * Hint: Use the process' _proc_sem, and the launcher's _done_sem to
+         * initiate a new request on the idle process, and ensure that there
          * exist idle processes.
          */
 
@@ -141,4 +144,8 @@ void process_pool_launcher::execute_request(request *req)
         assert(request::copy_size(req) <= RQST_BUF_SZ);
         assert(st != NULL);
         request::copy_request((char*)st->_request, req);
+
+        /*
+         * YOUR CODE HERE
+         */
 }
--- src/thread_pool_launcher.cc
+++ src/thread_pool_launcher.cc
@@ -12,37 +12,37 @@ thread_pool_launcher::thread_pool_launcher(int pool_sz)
         int i;
         pthread_t *thread;
         thread_state *states;
-        
+
         /* Initialize thread_pool_launcher fields */
         _pool_sz = pool_sz;
-        
-        /* 
+
+        /*
          * YOUR CODE HERE
-         * 
+         *
          * Initialize _done_sem and _free_list_sem using sem_init.
-         * 
-         * _free_list_sem protects the free-list of thread_states, which 
-         * corresponds to the list of idle threads. 
-         * 
+         *
+         * _free_list_sem protects the free-list of thread_states, which
+         * corresponds to the list of idle threads.
+         *
          * _done_sem is used to track the number of idle/busy processes.
-         * 
-         * Hint: Since both _free_list_sem, and _done_sem are shared among 
-         * threads of a single process, the second argument to sem_init must be 
-         * set appropriately. 
+         *
+         * Hint: Since both _free_list_sem, and _done_sem are shared among
+         * threads of a single process, the second argument to sem_init must be
+         * set appropriately.
          */
 
 
-        /* 
-         * Initialize thread_state structs. Each thread in the thread pool is 
-         * assigned a thread_state struct. 
+        /*
+         * Initialize thread_state structs. Each thread in the thread pool is
+         * assigned a thread_state struct.
          */
         states = (thread_state*)malloc(sizeof(thread_state)*pool_sz);
         for (i = 0; i < pool_sz; ++i) {
                 thread = (pthread_t*)malloc(sizeof(pthread_t));
-                
-                /* 
-                 * Each thread in the pool has a corresponding semaphore which 
-                 * is used to signal the thread to begin executing a new 
+
+                /*
+                 * Each thread in the pool has a corresponding semaphore which
+                 * is used to signal the thread to begin executing a new
                  * request. We initialize the value of this semaphore to 0.
                  */
                 sem_init(&states[i]._thread_sem, INTRA_PROC_SEM, 0);
@@ -53,10 +53,10 @@ thread_pool_launcher::thread_pool_launcher(int pool_sz)
                 states[i]._txns_executed = _txns_executed;
                 states[i]._list_ptr = &states[i+1];
                 states[i]._free_list = &_free_list;
-        }        
+        }
         states[i-1]._list_ptr = NULL;
         _free_list = states;
-        
+
         /*
          * YOUR CODE HERE
          *
@@ -66,16 +66,16 @@ thread_pool_launcher::thread_pool_launcher(int pool_sz)
 
 void thread_pool_launcher::execute_request(request *req)
 {
-        
+
         launcher::execute_request(req);
-        
-        /* 
+
+        /*
          * YOUR CODE HERE
          *
-         * Find an idle thread from the free-list, and execute the request on 
-         * the idle process. 
-         * 
-         * Hint: Use _thread_sem, and _done_sem to coordinate the execution of 
+         * Find an idle thread from the free-list, and execute the request on
+         * the idle process.
+         *
+         * Hint: Use _thread_sem, and _done_sem to coordinate the execution of
          * threads in the pool and the launcher.
          */
 }
@@ -83,25 +83,29 @@ void thread_pool_launcher::execute_request(request *req)
 void* thread_pool_launcher::executor_fn(void *arg)
 {
         thread_state *st;
-        
+
         st = (thread_state*)arg;
         while (true) {
                 /*
                  * YOUR CODE HERE
-                 * 
-                 * Wait for a new request, and execute it. After executing the 
-                 * request return thread_state to the launcher's thread_state 
-                 * free-list. Remember to signal/wait the appropriate 
+                 *
+                 * Wait for a new request, and execute it. After executing the
+                 * request return thread_state to the launcher's thread_state
+                 * free-list. Remember to signal/wait the appropriate
                  * _thread_sem, and _done_sem.
-                 * 
-                 * When your code is ready, remove the assert(false) statement 
+                 *
+                 * When your code is ready, remove the assert(false) statement
                  * below.
                  */
                 assert(false);
-                
+
                 /* exec request */
                 st->_req->execute();
                 fetch_and_increment(st->_txns_executed);
+
+                /*
+                 * YOUR CODE HERE
+                 */
         }
         return NULL;
 }
